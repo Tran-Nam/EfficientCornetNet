28280ec4d4a5a1f0a0a7c465a4ecc1d8318c8e0e
Tran-Nam <phuongnam9613@gmail.com>
2019-10-23 14:03:45 +0700

    add placeholder

--- train.py
+++ train.py
@@ -2,11 +2,12 @@ import tensorflow as tf
 import numpy as np
 from net.efficentnet import EfficentNet
 
-img = np.random.random((512, 512, 3))
-img = (img*255).astype('uint8')
-img = np.expand_dims(img, axis=0)
-print(img.shape)
-img = tf.convert_to_tensor(img, dtype='float32')
+_img = np.random.random((512, 512, 3))
+_img = (_img*255).astype('uint8')
+_img = np.expand_dims(_img, axis=0)
+print(_img.shape)
+# _img = tf.convert_to_tensor(_img, dtype='float32')
+img = tf.placeholder(tf.float32, shape=[None, 512, 512, 3])
 
 EfficentNet = EfficentNet()
 # init = tf.global_variables_initializer()
@@ -14,6 +15,11 @@ out = EfficentNet.net(img=img)
 writer = tf.summary.FileWriter('./graphs', tf.get_default_graph())
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    sess.run(out)
+    heatmap, offset = sess.run(out, feed_dict={img: _img})
+    # print(offset.shape) ### numpy array !!!!!!!!!!! 0_0
+    print(type(offset))
+    print(type(heatmap))
+    print(offset.shape, heatmap.shape)
+    
     
 print('Done!')
