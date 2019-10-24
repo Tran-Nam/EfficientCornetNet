846bf30d45affddf6e7bb4aeb16892c72a67f12b
Tran-Nam <phuongnam9613@gmail.com>
2019-10-24 21:26:04 +0700

    load images and labels

--- processing/dataloader.py
+++ processing/dataloader.py
@@ -7,7 +7,8 @@ import sys
 sys.path.append('..')
 
 from .augment import random_crop, rotate, add_noise, color_variance
-from helper.generate_gt import gen_heatmap
+from helper.generate_gt import gen_gt
+from helper.utils import resize
 
 class DataLoader(object):
     def __init__(self, image_dir, labels_path='../../data/labels.csv'):
@@ -17,7 +18,7 @@ class DataLoader(object):
         self.ds = tf.data.Dataset.list_files(os.path.join(image_dir, '*'))
         n_img = len(list(set(self.ds['filename'])))
 
-        self.BATCH_SIZE = 32
+        self.BATCH_SIZE = 4
         self.IMG_HEIGHT = 512
         self.IMG_WIDTH = 512
         self.STEP_PER_EPOCH = np.ceil(n_img/self.BATCH_SIZE)
@@ -35,11 +36,11 @@ class DataLoader(object):
                 pts_loc = 0
             elif row_c=='topright':
                 pts_loc = 1
-            elif row_c=='bottomright'
+            elif row_c=='bottomright':
                 pts_loc = 2
             elif row_c=='bottomleft':
                 pts_loc = 3
-            pts[pts_loc] = np.array([row['x'], row['y']).reshape[1, 2]
+            pts[pts_loc] = np.array([row['x'], row['y']]).reshape[1, 2]
         # pts = np.array(pts).reshape(4, 2)
         return pts
 
@@ -57,9 +58,17 @@ class DataLoader(object):
         return img
 
     def process_path(self, file_path):
-        heatmap, offset = self.get_labels(file_path)
+        # heatmap, offset = self.get_labels(file_path)
         img = tf.io.read_file(file_path)
         img = self.decode_img(img)
+
+        #################################
+        pts = self.get_corners(file_path)
+        # resize to 512x512
+        img, pts = resize(img, pts, side=512)
+        _, __, heatmap, offset = gen_gt(img, pts)
+
+
         return img, heatmap, offset
 
     def create_pairs(self):
@@ -82,9 +91,16 @@ class DataLoader(object):
         ds = ds.batch(self.BATCH_SIZE)
 
         # let dataset fetch batch in background when model is training
-        ds = ds.prefetch(buffer_size=AUTOTUNE)
+        ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
         return ds
 
     def next_batch(self):
         image_batch, label_batch = next(iter(self.train_ds))
+        return image_batch, label_batch
+
+if __name__=='__main__':
+    dataload = DataLoader('../../data/images')
+    dataload.create_pairs()
+    image_batch, label_batch = dataload.next_batch()
+    print(image_batch.shape)
