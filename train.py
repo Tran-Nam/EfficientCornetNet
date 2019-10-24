5ecd97d839c63b4c9901a402ef75701e2c31d13b
Tran-Nam <phuongnam9613@gmail.com>
2019-10-25 00:36:22 +0700

    add loss

--- train.py
+++ train.py
@@ -1,25 +1,45 @@
 import tensorflow as tf 
 import numpy as np
 from net.efficentnet import EfficentNet
+from helper.generate_gt import gen_gt
+from module.loss import offset_loss, focal_loss
 
-_img = np.random.random((512, 512, 3))
+_img = np.random.random((10, 512, 512, 3))
+_heatmap = np.random.random((10, 128, 128, 4))
+_offset = np.random.random((10, 128, 128, 2))
+_mask = np.zeros((10, 128, 128))
+_mask[:, 20:40, 50:60] = 1
 _img = (_img*255).astype('uint8')
-_img = np.expand_dims(_img, axis=0)
+# _img = np.expand_dims(_img, axis=0)
+# _heatmap = np.expand_dims(_heatmap, axis=0)
+# _offset = np.expand_dims(_offset, axis=0)
+# _mask = np.expand_dims(_mask, axis=0)
+
 print(_img.shape)
 # _img = tf.convert_to_tensor(_img, dtype='float32')
 img = tf.placeholder(tf.float32, shape=[None, 512, 512, 3])
+heatmap_gt = tf.placeholder(tf.float32, shape=[None, 128, 128, 4])
+offset_gt = tf.placeholder(tf.float32, shape=[None, 128, 128, 2])
+mask = tf.placeholder(tf.float32, shape=[None, 128, 128])
 
 EfficentNet = EfficentNet()
 # init = tf.global_variables_initializer()
-out = EfficentNet.net(img=img)
+heatmap, offset = EfficentNet.net(img=img)
+f_loss = focal_loss(heatmap, heatmap_gt)
+o_loss = offset_loss(offset, offset_gt, mask)
+total_loss = f_loss + o_loss
+# focal_loss = loss.focal_loss2(heatmap, img)
 writer = tf.summary.FileWriter('./graphs', tf.get_default_graph())
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    heatmap, offset = sess.run(out, feed_dict={img: _img})
+    heatmap, offset, loss = sess.run([heatmap, offset, total_loss], feed_dict={img: _img, heatmap_gt: _heatmap, offset_gt: _offset, mask: _mask})
+    
     # print(offset.shape) ### numpy array !!!!!!!!!!! 0_0
     print(type(offset))
     print(type(heatmap))
-    print(offset.shape, heatmap.shape)
+    # print(offset.shape, heatmap.shape)
+    # print(loss.eval())
+    print(loss)
     
     
 print('Done!')
